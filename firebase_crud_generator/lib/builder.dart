a20ae493e3675455d1e491d43ee300f674ae88b5
Tim Whiting <tim@whitings.org>
2020-02-08 11:55:36 -0700

    updates

---
+++ firebase_crud_generator/lib/builder.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2018, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:build/build.dart';
+
+import 'package:source_gen/source_gen.dart';
+import 'firebase_crud_generator.dart';
+
+Builder builtValue(BuilderOptions _) => SharedPartBuilder([FirebaseCRUDGenerator()], 'firebase_crud');
