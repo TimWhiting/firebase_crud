76dee9c7305f3baaab8174b5a7fff10457349fa0
Tim Whiting <tim@whitings.org>
2020-02-08 15:07:41 -0700

    updates

--- firebase_crud_generator/lib/src/firebase_crud_generator_base.dart
+++ firebase_crud_generator/lib/src/firebase_crud_generator_base.dart
@@ -4,9 +4,10 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
 void writeCRUDMethods(List<PropertyAccessorElement> getters, String collection, String className, StringBuffer buffer) {
-  buffer.writeln('''extension ToMap on $className {
+  buffer.writeln('''extension ${className}ToMap on $className {
         Map<String, dynamic> get asFirebaseMap {
           final jsonMap = serializers.serialize(this, specifiedType:$className) as Map<String, dynamic>;
+          //${getters.where((getter) => getter.metadata.any((annotation) => annotation.element.displayName == "FirebaseRef")).toList()}
           return jsonMap;
         }
       }
