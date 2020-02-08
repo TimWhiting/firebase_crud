5883ff37ce32a06db851a4d2b960cafbd5a9c849
Tim Whiting <tim@whitings.org>
2020-02-08 09:57:50 -0700

    updates

--- firebase_crud/lib/src/firebase_crud_base.dart
+++ firebase_crud/lib/src/firebase_crud_base.dart
@@ -1,10 +1,10 @@
 class FirebaseCRUD {
   final String collection;
   final bool copyID;
-  const FirebaseCRUD(this.collection, this.copyID);
+  const FirebaseCRUD({this.collection, this.copyID});
 }
 
 class FirebaseRef {
   final String collection;
-  const FirebaseRef(this.collection);
+  const FirebaseRef({this.collection});
 }
--- firebase_crud_generator/lib/src/firebase_crud_generator_base.dart
+++ firebase_crud_generator/lib/src/firebase_crud_generator_base.dart
@@ -1,1 +1,7 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:firebase_crud/firebase_crud.dart';
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
 
+void writeCRUDMethods(
+    List<PropertyAccessorElement> getters, bool copyID, String collection, String className, StringBuffer buffer) {}
