89c311d2624960738e4945f1ef16d8bcfe086352
Tim Whiting <tim@whitings.org>
2020-02-08 11:51:26 -0700

    updates

--- firebase_crud/lib/src/firebase_crud_base.dart
+++ firebase_crud/lib/src/firebase_crud_base.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 class FirebaseCRUD {
   final String collection;
   const FirebaseCRUD({this.collection});
@@ -7,3 +10,13 @@ class FirebaseRef {
   final String collection;
   const FirebaseRef({this.collection});
 }
+
+abstract class FirestoreRepository {
+  final Firestore _firestore;
+  FirestoreRepository({firestore}) : _firestore = firestore;
+}
+
+// abstract class FirestoreData<T> {
+//   T get firestoreData;
+//   set firestoreData(T data);
+// }
