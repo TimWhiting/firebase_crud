ae82bf27b8b234203bd8f4ced61f3727cf26922c
Tim Whiting <tim@whitings.org>
2020-02-08 16:01:01 -0700

    updates

--- firebase_crud_generator/lib/src/firebase_crud_generator_base.dart
+++ firebase_crud_generator/lib/src/firebase_crud_generator_base.dart
@@ -4,12 +4,38 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
 void writeCRUDMethods(List<PropertyAccessorElement> getters, String collection, String className, StringBuffer buffer) {
+  final fields = getters
+      .where((getter) => getter.metadata.any(
+            (annotation) => annotation.element.enclosingElement.name == "FirebaseRef",
+          ))
+      .toList();
+  final fieldReferenceExtractors = fields
+      .map((field) =>
+          'final ${field.name}Field = ${field.type.returnType.getDisplayString()}FirebaseReference(this.${field.name}).firebaseRef(_firebase);')
+      .join('\n');
+  final fieldSettors = fields.map((field) => 'jsonMap["${field.name}"] = ${field.name}Field;').join('\n');
   buffer.writeln('''extension ${className}ToMap on $className {
-        Map<String, dynamic> get asFirebaseMap {
+        Map<String, dynamic> asFirebaseMap(_firebase) {
           final jsonMap = serializers.serialize(this, specifiedType:FullType($className)) as Map<String, dynamic>;
-          //${getters.map((getter) => getter.metadata.join()).join('\n')}
-          //${getters.where((getter) => getter.metadata.any((annotation) => annotation.element.displayName == "FirebaseRef")).toList()}
+          $fieldReferenceExtractors
+          $fieldSettors
           return jsonMap;
+        }
+      }
+    ''');
+  buffer.writeln('''extension ${className}FirebaseReference on $className {
+        DocumentReference firebaseRef(_firebase) {
+          return _firebase.collection('$collection').document(this.id);
+        }
+      }
+    ''');
+
+  buffer.writeln('''extension ${className}Reference on DocumentReference {
+        Future<$className> fromFirebaseReference() async {
+          final document = await this.get();
+          final Map<String, dynamic> jsonMap = document.data;
+          jsonMap['id'] = document.documentID;
+          return serializers.deserialize(jsonMap, specifiedType: FullType($className)) as $className;
         }
       }
     ''');
